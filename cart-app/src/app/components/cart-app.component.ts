fbb7887c33f699ef84aead8b7f4ee6855c4a32c1
reginardzss <84197750+reginardzss@users.noreply.github.com>
2024-08-12 12:53:57 -0600

    cart component

--- cart-app/src/app/components/cart-app.component.ts
+++ cart-app/src/app/components/cart-app.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../../models/cartItem';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent, CartComponent],
   templateUrl: './cart-app.component.html',
 })
 export class CartAppComponent implements OnInit{
   products: Product[] = [];
+  items: CartItem[] = [];
+
   constructor(private service: ProductService){}
   
   ngOnInit(): void {
