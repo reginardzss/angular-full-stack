691cb0b0b6025881206b2388a1662ee0849cb6a8
reginardzss <84197750+reginardzss@users.noreply.github.com>
2024-08-12 12:33:47 -0600

    division en subcomponentes

--- cart-app/src/app/components/cart-app.component.ts
+++ cart-app/src/app/components/cart-app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart-app.component.html',
 })
-export class CartAppComponent {
-  productos: Product[] = [];
-  constructor(private service: ProductService){
-
+export class CartAppComponent implements OnInit{
+  products: Product[] = [];
+  constructor(private service: ProductService){}
+  
+  ngOnInit(): void {
+    this.products = this.service.findAll();
   }
 }
